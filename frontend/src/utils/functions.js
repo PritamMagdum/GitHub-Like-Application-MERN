5e7b88b663481857a600d15e14f68ef527b4df1e
Pritam <pritammagdum24@gmail.com>
2024-03-30 12:13:35 +0530

    Added Cutom Function for Format the Date

--- frontend/src/utils/functions.js
+++ frontend/src/utils/functions.js
@@ -6,3 +6,44 @@ export function formatMemberSince(inputDateString) {
   );
   return formattedDate;
 }
+
+export function formatDate(inputDateString) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const date = new Date(inputDateString);
+  const monthName = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  function getOrdinalSuffix(day) {
+    if (day >= 11 && day <= 13) {
+      return day + "th";
+    }
+    switch (day % 10) {
+      case 1:
+        return day + "st";
+      case 2:
+        return day + "nd";
+      case 3:
+        return day + "rd";
+      default:
+        return day + "th";
+    }
+  }
+
+  const formattedDate = `${monthName} ${getOrdinalSuffix(day)}, ${year}`;
+  return formattedDate;
+}
