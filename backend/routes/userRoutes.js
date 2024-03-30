77e291fbc1d11f19e6f7f88d0f8634e260ed2496
Pritam <pritammagdum24@gmail.com>
2024-03-30 22:08:38 +0530

    Added Like Profile Endpoint or API to Likes the User Profiles if User Wanna Like

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUserProfileAndRepos } from "../controllers/userController.js";
+import {
+  getUserProfileAndRepos,
+  likeProfile,
+} from "../controllers/userController.js";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileAndRepos);
 //TODO: two routes - 1)get likes 2) get post like a profile
+router.post("/like/:username", ensureAuthenticated, likeProfile);
 
 export default router;
