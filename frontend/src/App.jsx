1014e9e8f59e884de4a9436ccc2dba4be24bbe0d
Pritam <pritammagdum24@gmail.com>
2024-03-30 21:40:07 +0530

    Handled User Routes with or without Valid User

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -17,10 +17,22 @@ function App() {
       <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/likes" element={<LikesPage />} />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/explore"
+            element={authUser ? <ExplorePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/likes"
+            element={authUser ? <LikesPage /> : <Navigate to="/login" />}
+          />
         </Routes>
         <Toaster />
       </div>
