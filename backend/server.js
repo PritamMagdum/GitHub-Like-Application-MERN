cd50a8959ab15b2ab157773ace474d7c4606d8f0
Pritam <pritammagdum24@gmail.com>
2024-03-30 16:54:55 +0530

    Added Explore Routes and Middleware for Explore the Top Repos

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import exploreRoutes from "./routes/exploreRoutes.js";
 import cors from "cors";
 const app = express();
 dotenv.config();
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
 // Middlewares
 app.use(cors());
 app.use("/api/users", userRoutes);
+app.use("/api/explore", exploreRoutes);
 
 app.listen(8080, () => {
   console.log("server started at port 8080");
