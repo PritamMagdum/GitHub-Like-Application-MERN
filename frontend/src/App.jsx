b477677821cf04d4412a4bdb720690f4c785a24e
Pritam <pritammagdum24@gmail.com>
2024-03-30 21:48:08 +0530

    Handled Redirecting Home Issue when Checking User Validate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,10 @@ import Sidebar from "./components/Sidebar";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const { authUser } = useAuthContext();
-  console.log("authenticated user -->", authUser);
+  const { authUser, loading } = useAuthContext();
+  // console.log("authenticated user -->", authUser);
+  if (loading) return null;
+
   return (
     <div className="flex">
       <Sidebar />
