b2ba4e1c2b44f25199c8647c42378d3c3ac3dee0
Pritam <pritammagdum24@gmail.com>
2024-03-30 16:56:58 +0530

    Added Functionality to Fetch the Explore Repos from Backend

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -13,19 +13,12 @@ const ExplorePage = () => {
     setLoading(true);
     setRepos([]);
     try {
-      // const res = await fetch("/api/explore/repos/" + language);
       const res = await fetch(
-        `https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=10`,
-        {
-          headers: {
-            authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-          },
-        }
+        `http://localhost:8080/api/explore/repos/${language}`
       );
-      // const { repos } = await res.json();
-      // setRepos(repos);
-      const data = await res.json();
-      setRepos(data.items);
+
+      const { repos } = await res.json();
+      setRepos(repos);
       setSelectedLanguage(language);
     } catch (error) {
       toast.error(error.message);
