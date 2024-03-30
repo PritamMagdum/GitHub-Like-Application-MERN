1522e9db5f89b317bc21fa8ec191056abd335a14
Pritam <pritammagdum24@gmail.com>
2024-03-30 09:44:36 +0530

    Added Routes for All Pages with the Help of React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-green-500">Welcome from tailwind css</h1>
-    </>
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
