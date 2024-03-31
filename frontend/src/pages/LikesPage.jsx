243ae3702dae40c3e150458330b25536bf62c4e4
Pritam <pritammagdum24@gmail.com>
2024-03-31 09:18:33 +0530

    Added Functionality to Show the Likes Profiles to the Likes Page

--- frontend/src/pages/LikesPage.jsx
+++ frontend/src/pages/LikesPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import toast from "react-hot-toast";
+import { formatDate } from "../utils/functions.js";
 
 const LikesPage = () => {
   const [likes, setLikes] = useState([]);
@@ -39,35 +40,35 @@ const LikesPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-glass border-b">
-            <td className="w-4 p-4">
-              <div className="flex items-center">
-                <span>1</span>
-              </div>
-            </td>
-            <th
-              scope="row"
-              className="flex items-center px-6 py-4 whitespace-nowrap "
-            >
-              <img
-                className="w-10 h-10 rounded-full"
-                src={
-                  "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-                }
-                alt="Jese image"
-              />
-              <div className="ps-3">
-                <div className="text-base font-semibold">dasdas</div>
-              </div>
-            </th>
-            <td className="px-6 py-4">das</td>
-            <td className="px-6 py-4">
-              <div className="flex items-center">
-                <FaHeart size={22} className="text-red-500 mx-2" />
-                Liked your profile
-              </div>
-            </td>
-          </tr>
+          {likes.map((user, index) => {
+            <tr className="bg-glass border-b" key={index}>
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <span>{index + 1}</span>
+                </div>
+              </td>
+              <th
+                scope="row"
+                className="flex items-center px-6 py-4 whitespace-nowrap "
+              >
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={user.avatarUrl}
+                  alt="User Image"
+                />
+                <div className="ps-3">
+                  <div className="text-base font-semibold">{user.username}</div>
+                </div>
+              </th>
+              <td className="px-6 py-4">{formatDate(user.likedDate)}</td>
+              <td className="px-6 py-4">
+                <div className="flex items-center">
+                  <FaHeart size={22} className="text-red-500 mx-2" />
+                  Liked your profile
+                </div>
+              </td>
+            </tr>;
+          })}
         </tbody>
       </table>
     </div>
