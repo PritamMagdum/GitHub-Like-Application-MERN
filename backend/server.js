d17976dbba5b4574e511fe5022350c889a8e3368
Pritam <pritammagdum24@gmail.com>
2024-03-31 09:56:07 +0530

    Added Directory Path with the Help of Path Package for Deployed Server Understanding

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,15 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import passport from "passport";
 import session from "express-session";
 import "./passport/githubAuth.js";
+import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("server is ready");
-});
+const __dirname = path.resolve();
+
+// console.log(__dirname);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -30,7 +32,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.listen(8080, () => {
-  console.log("server started at port 8080");
+  console.log(`server started at port ${PORT}`);
   connectMongoDB();
 });
